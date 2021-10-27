d7d18eae0d54932dc131241f6d858f8fde8f893c
RedoneRony <chokers547@gmail.com>
2021-10-27 15:15:34 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: "AIzaSyCUNNbpZM3rsHPB47B_QWsx5Fd0oyJb6VA",
+  authDomain: "ema-jon-ecommerce.firebaseapp.com",
+  projectId: "ema-jon-ecommerce",
+  storageBucket: "ema-jon-ecommerce.appspot.com",
+  messagingSenderId: "63682256468",
+  appId: "1:63682256468:web:99083653ac4de61a9557c1",
+};
+
+export default firebaseConfig;
